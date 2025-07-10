7fd94c3b348a549a3a73e1c595b7b64346e11b69
Oluwatobiju Judah <tobijudah@gmail.com>
2025-07-10 22:10:38 +0100

    chore: added comments for desktop and mobile layout sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,6 +11,7 @@ import MobileHeader from "./components/MobileHeader";
 export default function HomePage() {
   return (
     <div className="flex flex-col">
+      {/* Desktop */}
       <div className="hidden min-h-screen lg:flex">
         <GridLayout>
           <Header />
@@ -18,6 +19,7 @@ export default function HomePage() {
         </GridLayout>
       </div>
 
+      {/* Mobile */}
       <div className="flex h-screen flex-col lg:hidden">
         <MobileGridLayout>
           <MobileHeader />
