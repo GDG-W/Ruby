2e424bfba014e8c1a85897d99c2785e0bd9132ff
Sodiq Akinjobi <sodiq.akinjobi@gmail.com>
2025-09-01 23:30:14 +0100

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -26,12 +26,12 @@ export default function Header() {
             className="absolute -left-38 -z-10"
           />
           <div className="flex h-full">
-            <Link href="https://2024.devfestlagos.com/" target="_blank">
+            <Link href="https://devfestlagos.com/earlybird" target="_blank">
               <Button
                 variant="primary"
                 className="flex h-full items-center justify-center gap-2 bg-[#F6B51E] px-3 text-xs whitespace-nowrap transition-all hover:bg-[#E5A818] sm:px-4 sm:text-sm lg:px-6 lg:text-base"
               >
-                Visit 2024 Website
+                Get Early Bird Tickets
                 <Image src="/vector.svg" alt="arrow icon" width={14} height={14} />
               </Button>
             </Link>
