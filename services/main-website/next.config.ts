f7dbdb1de426ac1993741aba18176c4e40b31d7e
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-09-04 13:31:22 +0100

    Enable importing of svgs as react components

--- services/main-website/next.config.ts
+++ services/main-website/next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    useCache: true,
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
+  },
   sassOptions: {
     additionalData: `@import "src/styles/common.scss";`,
   },
