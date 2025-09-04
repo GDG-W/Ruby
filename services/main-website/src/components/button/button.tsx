be7f935f595a6f0b29f2648598b236e0d3372595
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-09-04 13:31:08 +0100

    tweak button styling

--- services/main-website/src/components/button/button.tsx
+++ services/main-website/src/components/button/button.tsx
@@ -3,9 +3,9 @@ import styles from "./button.module.scss";
 import clsx from "clsx";
 import NextLink, { type LinkProps } from "next/link";
 
-type BaseProps = { children: React.ReactNode; hideBorder?: boolean; className?: string };
+type BaseProps = { children: React.ReactNode; size?: "sm" | "default"; className?: string };
 
-type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & { type: "button" };
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & { type?: "button" };
 type NextLinkProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & { type: "a"; to: string };
 type ExternalLinkProps = LinkProps & { type: "link"; };
 
@@ -14,15 +14,9 @@ type Props = BaseProps & (ButtonProps | NextLinkProps | ExternalLinkProps);
 export function Button(props: Props) {
   const { children } = props;
 
-  const className = clsx(styles.button, props.hideBorder && styles.buttonNoBorder, props.className);
+  const className = clsx(styles.button, props.className, props.size === "sm" && styles.small);
 
   switch (props.type) {
-    case "button":
-      return (
-        <button {...props} className={className}>
-          {children}
-        </button>
-      );
     case "link":
       return (
         <NextLink {...props} className={className}>
@@ -34,6 +28,12 @@ export function Button(props: Props) {
         <a {...props} className={className}>
           {children}
         </a>
+      );
+    default:
+      return (
+        <button {...props} className={className}>
+          {children}
+        </button>
       );
   }
 }
