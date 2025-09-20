111929baa997d961a6472cbd79bcfdb15bc35aa8
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-09-20 12:10:23 +0100

    Implemented top hero with curves images

--- services/main-website/src/components/homepage/hero/hero.tsx
+++ services/main-website/src/components/homepage/hero/hero.tsx
@@ -1,7 +1,35 @@
 import styles from "./hero.module.scss";
+import DLogo from "@/assets/devfest-logo.svg";
+import heroImage from "@/assets/hero-image.png";
+import heroImageMobile from "@/assets/hero-image-mobile.png";
+import Image from "next/image";
 
-export function HomepageHero(){
+export function HomepageHero() {
   return (
-    <h1 className={styles.hero}>Devfest lagos</h1>
+    <section className={styles.hero}>
+      <div className={styles.devfestText}>
+        Devfest
+        <div className={styles.lagosRow}>
+          <DLogo />
+          <span>Lagos</span>
+        </div>
+      </div>
+      <h1 className={styles.yearText}>
+        2025
+      </h1>
+      <Image
+        src={heroImage}
+        className={styles.heroImage}
+        alt="Clipped illustration"
+        style={{ width: "100%" }}
+      />
+      <Image
+        src={heroImageMobile}
+        className={styles.heroImageMobile}
+        alt="Clipped illustration"
+        style={{ width: "100%" }}
+      />
+
+    </section>
   )
 }
