c0dd85a0d0899c117c5f1a40532dc20b06019415
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-09-20 12:10:14 +0100

    Add mobile view for navbar

--- services/main-website/src/components/navbar/navbar.tsx
+++ services/main-website/src/components/navbar/navbar.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import styles from "./navbar.module.scss";
 import NavLogo from "@/assets/nav-logo.svg";
 import { Button } from "../button/button";
+import HamburgerIcon from "@/assets/hamburger.svg";
+import CloseIcon from "@/assets/close.svg";
+import { useState } from "react";
+import clsx from "clsx";
 
 const links = [
   {
@@ -23,25 +29,56 @@ const links = [
 ]
 
 export function Navbar() {
+  const [navOpen, setNavOpen] = useState(false);
+
   return (
-    <nav className={styles.navbar}>
-      <Link href="/" className={styles.logo}>
-        <NavLogo />
-      </Link>
-      <ul className={styles.links}>
-        {
-          links.map(link => (
-            <li key={link.link}>
-              <Link href={link.link} className={styles.link}>
-                {link.text}
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
-      <Button size="sm">
-        BUY TICKETS
-      </Button>
-    </nav>
+    <>
+      <nav className={styles.navbar}>
+        <Link href="/" className={styles.logo}>
+          <NavLogo />
+        </Link>
+        <ul className={styles.links}>
+          {
+            links.map(link => (
+              <li key={link.link}>
+                <Link href={link.link} className={styles.link}>
+                  {link.text}
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+        <Button size="sm" className={styles.buyTicket}>
+          BUY TICKETS
+        </Button>
+        <Button className={styles.mobileNavButton} onClick={() => setNavOpen(true)}>
+          <HamburgerIcon />
+        </Button>
+      </nav>
+      <aside className={clsx(styles.mobileNav, { [styles.open]: navOpen })}>
+        <div className={styles.topRow}>
+          <div className={styles.logo}>
+            <NavLogo />
+          </div>
+          <Button className={styles.mobileNavClose} onClick={() => setNavOpen(false)}>
+            <CloseIcon />
+          </Button>
+        </div>
+        <ul className={styles.links}>
+          {
+            links.map(link => (
+              <li key={link.link}>
+                <Link href={link.link} className={styles.link}>
+                  {link.text}
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+        <Button className={styles.buyTicket}>
+          BUY TICKETS
+        </Button>
+      </aside>
+    </>
   )
 }
