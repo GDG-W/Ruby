f37af5450cdd42e77f5bd6de94ecb52f9fdce72b
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-09-25 12:39:31 +0100

    Secure tickets

--- services/main-website/src/app/page.tsx
+++ services/main-website/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HomepageHero } from "@/components/homepage/hero/hero";
 import styles from "./page.module.scss";
+import { SecureTicket } from "@/components/homepage/secure-ticket/secure-ticket";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <HomepageHero />
+      <SecureTicket />
     </div>
   );
 }
