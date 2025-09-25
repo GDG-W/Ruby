ef88f6ac93f526b77eac3d91e9768d11795da85a
Oluwanifemi Adeyemi <deyeminifemi@gmail.com>
2025-09-25 13:45:17 +0100

    Mobile view for tracks

--- services/main-website/src/app/page.tsx
+++ services/main-website/src/app/page.tsx
@@ -1,12 +1,14 @@
 import { HomepageHero } from "@/components/homepage/hero/hero";
 import styles from "./page.module.scss";
 import { SecureTicket } from "@/components/homepage/secure-ticket/secure-ticket";
+import { Tracks } from "@/components/homepage/tracks/tracks";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <HomepageHero />
       <SecureTicket />
+      <Tracks />
     </div>
   );
 }
