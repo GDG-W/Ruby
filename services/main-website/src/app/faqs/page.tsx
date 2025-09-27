fb376c5416b025f0255da5bf5601dd99f5425cac
Oluwatobiju Judah <tobijudah@gmail.com>
2025-09-27 23:00:21 +0100

    fix: update FAQ tab from devfest-lagos-2025 to general

--- services/main-website/src/app/faqs/page.tsx
+++ services/main-website/src/app/faqs/page.tsx
@@ -2,5 +2,5 @@ import React from "react";
 import TabPage from "./[tab]/page";
 
 export default function FAQRoot() {
-  return <TabPage params={{ tab: "devfest-lagos-2025" }} />;
+  return <TabPage params={{ tab: "general" }} />;
 }
