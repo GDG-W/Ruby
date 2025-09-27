d79adbc83a37dab3d8a7c553ef094c73b24f3a67
Oluwatobiju Judah <tobijudah@gmail.com>
2025-09-27 07:46:04 +0100

    feat: adds GDG component with styling and content

--- services/main-website/src/app/page.tsx
+++ services/main-website/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HomepageHero } from "@/components/homepage/hero/hero";
 import styles from "./page.module.scss";
+import GDG from "@/components/homepage/gdg/gdg";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <HomepageHero />
+      <GDG />
     </div>
   );
 }
---
+++ services/main-website/src/components/homepage/gdg/gdg.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styles from "./gdg.module.scss";
+import Image from "next/image";
+import { Button } from "@/components/button/button";
+
+const GDG = () => {
+  return (
+    <div className={styles.gdg}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <h2 className={styles.title}>What is GDG Lagos up to?</h2>
+          <p className={styles.description}>
+            Stay updated on all our activities, events, webinars. Be the first to know what the GDG
+            Lagos community is up to.
+          </p>
+          <Button type="link" className={styles.button} href="https://gdg.community.dev/gdg-lagos/">
+            Join the community
+          </Button>
+        </div>
+        <Image
+          alt="GDG"
+          width={500}
+          height={538}
+          className={styles.image}
+          src="/images/GDG-Lagos-orbit.svg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default GDG;
