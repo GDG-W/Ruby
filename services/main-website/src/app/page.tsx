a16895749dfe2d8ca9b289c9c5c8963fff73267f
Oluwatobiju Judah <tobijudah@gmail.com>
2025-09-27 10:25:05 +0100

    feat: add Throwback component with styling and images

--- services/main-website/src/app/page.tsx
+++ services/main-website/src/app/page.tsx
@@ -1,10 +1,12 @@
 import { HomepageHero } from "@/components/homepage/hero/hero";
 import styles from "./page.module.scss";
+import Throwback from "@/components/homepage/throwback/throwback";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <HomepageHero />
+      <Throwback />
     </div>
   );
 }
