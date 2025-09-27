bbe5ed06f12255280e3df0b2c1b4f8cce5010c70
Oluwatobiju Judah <tobijudah@gmail.com>
2025-09-27 12:00:09 +0100

    feat: create FAQ item component with styling and integrate into FAQs list

---
+++ services/main-website/src/components/faq-item/faq-item.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styles from "./faq-item.module.scss";
+
+interface FaqItemProps {
+  id: number;
+  question: string;
+  answer: string;
+}
+
+const FaqItem: React.FC<FaqItemProps> = ({ id, question, answer }) => {
+  return (
+    <li className={styles.faq}>
+      <details className={styles.detail} name="faqs" id={`faq-${id}`}>
+        <summary className={styles.summary}>
+          <span className={styles.question}>{question}</span>
+          <div className={styles.toggle}>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M13 6V18.6H11.2V6H13Z" fill="#171717" />
+              <path d="M6 11H18.6V12.8H6V11Z" fill="#171717" />
+            </svg>
+          </div>
+        </summary>
+        <p className={styles.answer}>{answer}</p>
+      </details>
+      <svg
+        width="44"
+        height="33"
+        fill="none"
+        viewBox="0 0 44 33"
+        className={styles.polygon}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fill="currentColor"
+          d="M25.2295 30.4008C23.6316 32.5873 20.3684 32.5873 18.7705 30.4009L1.53343 6.81497C-0.398024 4.17213 1.4895 0.454788 4.7629 0.454788H39.2371C42.5105 0.454788 44.398 4.17212 42.4666 6.81497L25.2295 30.4008Z"
+        />
+      </svg>
+      <div className={styles.polygonCover} />
+    </li>
+  );
+};
+
+export default FaqItem;
--- services/main-website/src/components/homepage/faqs/faqs.tsx
+++ services/main-website/src/components/homepage/faqs/faqs.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./faqs.module.scss";
 import Image from "next/image";
 import { Button } from "@/components/button/button";
+import FaqItem from "@/components/faq-item/faq-item";
 
 const faqs = [
   {
@@ -47,41 +48,8 @@ const FAQs = () => {
               From registration to what to expect on the day, our FAQs have all the info you need.
             </p>
             <ul className={styles.list}>
-              {faqs.map((faq) => (
-                <li key={faq.id} className={styles.faq}>
-                  <details className={styles.detail} name="faqs" id={`faq-${faq.id}`}>
-                    <summary className={styles.summary}>
-                      <span className={styles.question}>{faq.question}</span>
-                      <div className={styles.toggle}>
-                        <svg
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <path d="M13 6V18.6H11.2V6H13Z" fill="#171717" />
-                          <path d="M6 11H18.6V12.8H6V11Z" fill="#171717" />
-                        </svg>
-                      </div>
-                    </summary>
-                    <p className={styles.answer}>{faq.answer}</p>
-                  </details>
-                  <svg
-                    width="44"
-                    height="33"
-                    fill="none"
-                    viewBox="0 0 44 33"
-                    className={styles.polygon}
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M25.2295 30.4008C23.6316 32.5873 20.3684 32.5873 18.7705 30.4009L1.53343 6.81497C-0.398024 4.17213 1.4895 0.454788 4.7629 0.454788H39.2371C42.5105 0.454788 44.398 4.17212 42.4666 6.81497L25.2295 30.4008Z"
-                    />
-                  </svg>
-                  <div className={styles.polygonCover} />
-                </li>
+              {faqs.map(({ id, question, answer }) => (
+                <FaqItem key={id} id={id} question={question} answer={answer} />
               ))}
             </ul>
             <Button type="link" href="/faqs" className={styles.button}>
