1796925dcc72683332047eca06eef165f12748c8
Oluwatobiju Judah <tobijudah@gmail.com>
2025-09-28 10:27:11 +0100

    fix: update alt text for images in throwback

--- services/main-website/src/components/homepage/throwback/throwback.tsx
+++ services/main-website/src/components/homepage/throwback/throwback.tsx
@@ -13,17 +13,17 @@ const Throwback = () => {
               width={765}
               height={379}
               quality={100}
-              alt="throwback"
               className={styles.tweetOne}
               src="/images/@dev_leoo-tweet.png"
+              alt="@dev_leoo's tweet on DevFest Lagos 2024 saying: 'No tech event in the country beats #DevFestLagos!Good job team @gdglagos #DevFestLagos2024'"
             />
             <Image
               width={488}
               height={488}
               quality={100}
-              alt="throwback"
               className={styles.photoOne}
               src="/images/devfest-2024-dancing.png"
+              alt="Some attendees of DevFest Lagos 2024 dancing during the after-party"
             />
           </div>
           <div className={styles.content}>
@@ -73,17 +73,17 @@ const Throwback = () => {
               width={750}
               height={413}
               quality={100}
-              alt="throwback"
               className={styles.tweetTwo}
               src="/images/@Motushbae-tweet.png"
+              alt="@Motushbae's tweet on DevFest Lagos 2024 saying: 'Last Saturday, I had an incredible time at #DevFestLagos leading a codelab on OAuth 2.0. I walked participants through the concepts and hands-on implementation, it was rewarding to see so much interaction and learning. Thanks to everyone who came through—y’all made it engaging.'"
             />
             <Image
               width={517}
               height={589}
               quality={100}
-              alt="throwback"
               className={styles.photoTwo}
               src="/images/devfest-2024-trio.png"
+              alt="Three attendees of DevFest Lagos 2024 at the DevFest photo booth"
             />
           </div>
         </div>
@@ -92,25 +92,25 @@ const Throwback = () => {
             width={750}
             height={380}
             quality={100}
-            alt="throwback"
             className={styles.tweetThree}
             src="/images/@adesay0-tweet.png"
+            alt="@adesay0's tweet on DevFest Lagos 2024 saying: 'My very first devfest. My &ldquo;devfirst&rdquo;. I had so much fun and met new people. I must be there next year *if I never japa sha*'"
           />
           <Image
             width={450}
             height={510}
             quality={100}
-            alt="throwback"
             className={styles.photoThree}
             src="/images/devfest-2024-speaker.png"
+            alt="John Ohio speaking at DevFest Lagos 2024 on 'Design Thinking for Problem-Solving: Beyond UX Design'"
           />
           <Image
             width={750}
             height={446}
             quality={100}
-            alt="throwback"
             className={styles.tweetFour}
             src="/images/@mutiatadebimpe-tweet.png"
+            alt="@mutiatadebimpe's tweet on DevFest Lagos 2024 saying: 'I had an incredible time at #DevFest2024! I met amazing people, gained valuable insights and had a blast! Huge thanks to the team for putting together such an amazing event!@gdglagos #DevFestLagos #Networking #Techcommunity #GrowthJourney'"
           />
         </div>
       </div>
