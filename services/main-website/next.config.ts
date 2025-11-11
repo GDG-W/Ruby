abbff7d1b6160da00f41a3beaa3dfc3938eb68be
Oluwatobiju Judah <tobijudah@gmail.com>
2025-11-11 21:11:57 +0100

    feat: add Google Cloud Storage as an image source in Next.js configuration

--- services/main-website/next.config.ts
+++ services/main-website/next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
+      { protocol: "https", hostname: "storage.googleapis.com", pathname: "/**" },
     ],
   },
   experimental: {
