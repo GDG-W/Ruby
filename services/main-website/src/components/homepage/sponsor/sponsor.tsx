2e7e9e05b41f4e1fbcf3431b25b08eca0baefdae
Oluwatobiju Judah <tobijudah@gmail.com>
2025-11-11 20:20:39 +0100

    feat: add Btrust sponsor SVG and integrate into homepage sponsor component

--- services/main-website/src/components/homepage/sponsor/sponsor.tsx
+++ services/main-website/src/components/homepage/sponsor/sponsor.tsx
@@ -9,6 +9,7 @@ import cleva from "@/assets/sponsors/cleva.png";
 import gdgBag from "@/assets/sponsors/gdg-bag.svg";
 import google from "@/assets/sponsors/google.png";
 import splitIcon from "@/assets/sponsors/split.png";
+import btrust from "@/assets/sponsors/btrust.png";
 import classes from "./sponsor.module.scss";
 
 const sponsorImages = [
@@ -17,6 +18,7 @@ const sponsorImages = [
   cleva,
   alat,
   cadana,
+  btrust,
   // techcabal,
   // shecodeafrica,
   // threemtt,
